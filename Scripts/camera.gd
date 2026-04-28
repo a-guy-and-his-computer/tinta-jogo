0908a2fb5f52205c450ec9bd6c17ebb4ff89a95f
Art-2005 <arvvv2023@gmail.com>
2026-04-28 15:59:52 -0300

    Atualizando câmera
    Câmera atualizada

--- Scripts/camera.gd
+++ Scripts/camera.gd
@@ -1,11 +1,14 @@
 extends Camera2D
 
+const SCREEN_WIDTH := 320.0
 var target: Node2D
-@export var follow_threshold_x: float = 0.0  
-@export var lerp_speed: float = 5.0           
+@export var follow_threshold_x: float = 0.0
+@export var lerp_speed: float = 5.0
+var left_limit: float = 0.0
 
 func _ready() -> void:
 	get_target()
+	left_limit = floor(target.global_position.x / SCREEN_WIDTH) * SCREEN_WIDTH
 
 func _process(delta: float) -> void:
 	if not target:
@@ -13,9 +16,15 @@ func _process(delta: float) -> void:
 
 	var player_screen_x = target.global_position.x - global_position.x
 
-	if player_screen_x > follow_threshold_x:
+	if target.global_position.x < left_limit:
+		var target_x = target.global_position.x + follow_threshold_x
+		global_position.x = lerp(global_position.x, target_x, lerp_speed * delta)
+		left_limit = global_position.x - SCREEN_WIDTH * 0.5
+
+	elif player_screen_x > follow_threshold_x:
 		var target_x = target.global_position.x - follow_threshold_x
 		global_position.x = lerp(global_position.x, target_x, lerp_speed * delta)
+		left_limit = global_position.x - SCREEN_WIDTH * 0.5
 
 	global_position.y = lerp(global_position.y, target.global_position.y, lerp_speed * delta)
 
