80d45681c3f6bd41aeee1121064a35486201f47f
joao <jmarcelopq@gmail.com>
2026-05-06 16:38:58 -0300

    fix: Consertei alguns problemas resultantes da mudança de pastas

--- Scripts/menu.gd
+++ Scripts/menu.gd
@@ -7,7 +7,7 @@ extends Control
 @onready var SOM_SPRITE: Sprite2D = $SomConfig/SomSprite
 
 func _on_button_pressed() -> void: # Jogar
-	get_tree().change_scene_to_file("res://Scenes/jogo.tscn")
+	get_tree().change_scene_to_file("res://Scenes/van_gogh.tscn")
 
 
 func _on_sair_pressed() -> void: # Sair
@@ -27,39 +27,39 @@ func _on_som_botao_pressed() -> void: # Som
 
 
 func _on_jogar_mouse_entered() -> void:
-	JOGAR_SPRITE.texture = load("res://Assets/Imagens/jogar_3.png")
+	JOGAR_SPRITE.texture = load("res://Assets/Imagens/Menus/jogar_3.png")
 
 
 func _on_jogar_mouse_exited() -> void:
-	JOGAR_SPRITE.texture = load("res://Assets/Imagens/jogar_2.png")
+	JOGAR_SPRITE.texture = load("res://Assets/Imagens/Menus/jogar_2.png")
 
 
 func _on_sair_mouse_entered() -> void:
-	SAIDA_SPRITE.texture = load("res://Assets/Imagens/saida_3.png")
+	SAIDA_SPRITE.texture = load("res://Assets/Imagens/Menus/saida_3.png")
 
 
 func _on_sair_mouse_exited() -> void:
-	SAIDA_SPRITE.texture = load("res://Assets/Imagens/saida_2.png")
+	SAIDA_SPRITE.texture = load("res://Assets/Imagens/Menus/saida_2.png")
 
 
 func _on_controles_botao_mouse_entered() -> void:
-	CONTROLES_SPRITE.texture = load("res://Assets/Imagens/celo_controles_2.png")
+	CONTROLES_SPRITE.texture = load("res://Assets/Imagens/Menus/celo_controles_2.png")
 
 func _on_controles_botao_mouse_exited() -> void:
-	CONTROLES_SPRITE.texture = load("res://Assets/Imagens/celo_controles_1.png")
+	CONTROLES_SPRITE.texture = load("res://Assets/Imagens/Menus/celo_controles_1.png")
 
 
 func _on_display_botao_mouse_entered() -> void:
-	DISPLAY_SPRITE.texture = load("res://Assets/Imagens/celo_video_2.png")
+	DISPLAY_SPRITE.texture = load("res://Assets/Imagens/Menus/celo_video_2.png")
 
 
 func _on_display_botao_mouse_exited() -> void:
-	DISPLAY_SPRITE.texture = load("res://Assets/Imagens/celo_video_1.png")
+	DISPLAY_SPRITE.texture = load("res://Assets/Imagens/Menus/celo_video_1.png")
 
 
 func _on_som_botao_mouse_entered() -> void:
-	SOM_SPRITE.texture = load("res://Assets/Imagens/celo_musica.png")
+	SOM_SPRITE.texture = load("res://Assets/Imagens/Menus/celo_musica.png")
 
 
 func _on_som_botao_mouse_exited() -> void:
-	SOM_SPRITE.texture = load("res://Assets/Imagens/celo_musica111.png")
+	SOM_SPRITE.texture = load("res://Assets/Imagens/Menus/celo_musica111.png")
