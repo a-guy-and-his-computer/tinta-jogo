0179e97453da4406e16be9d14469845790e52f09
Art-2005 <arvvv2023@gmail.com>
2026-05-07 20:44:14 -0300

    Adicionando tela de pausa
    tela de pausa

---
+++ Scripts/tela_pausa.gd
@@ -0,0 +1,23 @@
+extends CanvasLayer
+
+func _ready() -> void:
+	visible = false
+	get_tree().paused = false
+
+func _input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("pausar"):
+		if get_tree().paused:
+			visible = false
+			get_tree().paused = false
+		else:
+			visible = true
+			get_tree().paused = true
+
+
+func _on_button_pressed() -> void:
+	visible = false
+	get_tree().paused = false
+
+
+func _on_button_2_pressed() -> void:
+	get_tree().quit()
